996b47769915eb466ac72eed6ea1fe5ca356db52
Correa95 <mathewcorrea19@gmail.com>
2025-02-07 12:26:28 -0500

    working on form

--- inventory/views.py
+++ inventory/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
 
 # Create your views here.
 class Index(TemplateView):
     template_name = "inventory/index.html"
+
+class SignUp(View):
+    def get(self, request):
+        form = UserRegisterForm()
+        return render(request, "inventory/sign.html")
+
+
+    def post(self, request):
