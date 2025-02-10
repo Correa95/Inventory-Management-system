9f1dd424e1b65badcb49b42769a0f67deb79409b
Correa95 <mathewcorrea19@gmail.com>
2025-02-10 10:54:40 -0500

    fix the errors

--- inventory/urls.py
+++ inventory/urls.py
@@ -5,5 +5,6 @@ from .views import Index, SignUpView
 
 urlpatterns = [
     path("", Index.as_view(), name = "index" ),
-    path("signup/", SignUpView.as_view(), name="signup")
+    path("signup/", SignUpView.as_view(), name="signup"),
+    path("login/", SignUpView.as_view(), name="login")
 ]
