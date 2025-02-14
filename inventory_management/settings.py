72d58a3e48058b99492161133408b8535134a2ef
Correa95 <mathewcorrea19@gmail.com>
2025-02-14 10:51:34 -0500

    logout error fix

--- inventory_management/settings.py
+++ inventory_management/settings.py
@@ -129,6 +129,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "/dashboard"
+LOGIN_URL = "login"
 # LOGOUT_REDIRECT_URL = "index"
 # LOGOUT_REDIRECT_URL = "login"
 # LOGOUT_URL = "/logout/"
