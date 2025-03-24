4e8b29a659ccad1c3e5e462b9f17ea135dbd7db4
Correa95 <mathewcorrea19@gmail.com>
2025-03-23 21:29:28 -0400

    adding the delete function

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import Index, SignUpView, Dashboard, AddItem, EditItem
+from .views import Index, SignUpView, Dashboard, AddItem, EditItem, DeleteItem
 from django.contrib.auth import views as auth_views
 
 
@@ -9,6 +9,7 @@ urlpatterns = [
     path("dashboard/", Dashboard.as_view(), name = "dashboard"),
     path("addItem/", AddItem.as_view(), name="addItem"),
     path("editItem/<int:pk>", EditItem.as_view(), name="editItem"),
+    path("deleteItem/<int:pk>", DeleteItem.as_view(), name="deleteItem"),
     path("signup/", SignUpView.as_view(), name="signup"),
     path("login/", auth_views.LoginView.as_view(template_name = "inventory/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(template_name = "inventory/logout.html"), name = "logout"),
