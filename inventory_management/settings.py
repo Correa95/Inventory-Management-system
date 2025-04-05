9820e1af8b667bd9b760c1c20f8ed9bc511e1899
Correa95 <mathewcorrea19@gmail.com>
2025-04-04 22:20:12 -0400

    change the css directory

--- inventory_management/settings.py
+++ inventory_management/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'inventory/static'),  # Pointing to your app's static directory
+    os.path.join(BASE_DIR, 'static'),  # Pointing to your app's static directory
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
